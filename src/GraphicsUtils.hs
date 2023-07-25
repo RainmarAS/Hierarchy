5b85f670c2508d4b8fd8db2c04e8e612a700b21c
RainmarAS <aboinilf@gmail.com>
2023-07-25 19:04:04 +0300

    data ActsPosOnScreen replaced with Wolrd

--- src/GraphicsUtils.hs
+++ src/GraphicsUtils.hs
@@ -1,4 +1,4 @@
-module GraphicsUtils (drawing) where
+module GraphicsUtils (drawing,World) where
 import Graphics.Gloss
 import Graphics.Gloss.Data.Vector
 import Graphics.Gloss.Interface.Environment
@@ -6,10 +6,14 @@ import Data.List
 import Data.Word
 import Data.ByteString (ByteString, pack)
 
-colors = map (dark) $  cycle [red,green,rose, yellow, chartreuse, rose,  chartreuse, aquamarine, azure, violet]
+border_angle  = 1/2 :: Float
+arcRadius = 10000 :: Float
 
-drawing :: Int -> Int -> Int -> Picture
-drawing width height n  =  scale scaleX scaleY $ separateScreen $ ActorsPositionsOnScreen radiusV n --radiusV n
+data World = World {width :: Int, height :: Int, segments_quantity :: Int} deriving (Eq, Show)
+colors = map (dark) $  cycle [greyN 0.3]--,red,green,rose, yellow,chartreuse, rose,  chartreuse, aquamarine, azure, violet]
+
+drawing :: World-> Picture
+drawing  world@(World width height n)  =   separateScreen world -- scale scaleX scaleY $
        where 
               area = fromIntegral $ width * height :: Float
               radiusV = ( area / (2*pi*(fromIntegral n+1)) ) ** (1/2)  :: Float
@@ -19,40 +23,51 @@ drawing width height n  =  scale scaleX scaleY $ separateScreen $ ActorsPosition
 
 
 
-separateScreen actsPosOnScreen = rotate (90) $ pics
+separateScreen world = rotate (90) $ pics
        where 
-              radius' = radius actsPosOnScreen
-              n = parts_quantity actsPosOnScreen
+              n = segments_quantity world
               pics =  pictures $ circles -- ++ lines    ------> not needed yet
               --lines = map (\i -> line [(0,0),mulSV 1000 $ ithCosSin i]) [0..n-1]
               circles =    coloredSectors ++ [centerCircle] 
-              coloredSectors  = drawSectors actsPosOnScreen
+              coloredSectors  = drawSectors world
               degrees = 360/fromIntegral n
-              centerCircle = circleSolid (radius' )
+              centerCircle =  color (greyN 0.2) $ circleSolid (radius world)
               radians = 2*(pi::Double) /  fromIntegral n
               ithCosSin i = (realToFrac $ cos $ radians * fromIntegral i,realToFrac $ sin $ radians * fromIntegral i) 
 
 
-drawSectors actsPosOnScreen = coloredSectors
+drawSectors world = coloredSectors
        where  
-              radius' = radius actsPosOnScreen
-              n = parts_quantity actsPosOnScreen
+              n = segments_quantity world
               coloredSectors  =   zipWith (color) colors sectors
-              sectors = zipWith (<>) littleCircles thickArcs
+              sectors = zipWith (<>) thickArcs littleCircles
               radians = 2*(pi::Double) /  fromIntegral n
-              littleCircle = circleSolid (l * sin (realToFrac radians/2))
+              semgent_icon = color red $ circleSolid $ (min 0.9 $ (fromIntegral n)/10)*(l * sin (realToFrac radians/2) )
+              littleCircle_ = circleSolid $ (degrees - border_angle)/degrees*(l * sin (realToFrac radians/2) )
+              littleCircle = littleCircle_ <> semgent_icon
               littleCircles = zipWith (\(x,y) -> translate x y) (map ithCircleCenter [0..n-1]) (replicate n littleCircle)
-              l =  distanceToLittleCircle actsPosOnScreen--radius' / (1 - sin (realToFrac radians/2 )) :: Float
+              l =  distanceToLittleCircle world--radius' / (1 - sin (realToFrac radians/2 )) :: Float
               degrees = 360 /fromIntegral n
               thickArcs = map ithThickArc [0..n-1]
               --ithThickArc i = rotate ( degrees * fromIntegral (i+1) ) thArc
               ithCircleCenter i = mulSV l $ ithCosSinForCircles i
               ithCosSinForCircles i = (realToFrac $ cos $  radians *(1/2+ fromIntegral i) ,realToFrac $ sin $  radians *(1/2+ fromIntegral i) ) 
               --thArc = thickArc 0 degrees arcRadius (arcThickness actsPosOnScreen)
-              ithThickArc i = thickArc (degrees * i') (degrees*(i'+1)) arcRadius (arcThickness actsPosOnScreen)
-                     where 
-                            i' = fromIntegral i
+              ithThickArc i = thickArc (degrees * i'+border_angle/2) (degrees*(i'+1)-border_angle/2) arcRadius (arcThickness world)
+                     where i' = fromIntegral i
 
+radius (World width height n) =  ( area / (2*pi*(fromIntegral n+1)) ) ** (1/2)  :: Float
+          where area = fromIntegral $ width * height :: Float
+distanceToLittleCircle :: World -> Float
+distanceToLittleCircle world =  (radius world) / (1 - sin (realToFrac radians/2 ))
+       where
+              radians = 2*(pi::Double) /  fromIntegral (segments_quantity world)
+arcThickness :: World -> Float
+arcThickness world  =2* ( arcRadius - l * cos (realToFrac radians/2))
+       where 
+              l = distanceToLittleCircle world
+              radians = 2*(pi::Double) /  fromIntegral (segments_quantity world)
+{-          
 data ActorsPositionsOnScreen = ActorsPositionsOnScreen {radius :: Float, parts_quantity :: Int} deriving (Eq, Show)
 distanceToLittleCircle :: ActorsPositionsOnScreen -> Float
 distanceToLittleCircle actsPosOnScreen =  radius' / (1 - sin (realToFrac radians/2 ))
@@ -64,5 +79,5 @@ arcThickness actsPosOnScreen  =2* ( arcRadius - l * cos (realToFrac radians/2))
        where 
               l = distanceToLittleCircle actsPosOnScreen
               radians = 2*(pi::Double) /  fromIntegral (parts_quantity actsPosOnScreen)
-arcRadius ::  Float
-arcRadius = 10000
+
+-}
